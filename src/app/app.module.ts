0a544610cde355c8af49176ed714b3a3438f3a62
extajadhav <extajadhav@sidf.gov.sa>
2022-12-28 02:04:00 +0530

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AddCandidateDialogComponent } from './add-candidate-dialog/add-candidate-dialog.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -14,7 +13,6 @@ import { EmpListComponent } from './emp-list/emp-list.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatIconModule,
     MatDialogModule,
     FormsModule,
     ReactiveFormsModule,
